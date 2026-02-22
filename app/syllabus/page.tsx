7126ce48b7cb48d307283ca43c1ade3a5ba26f48
Capttech <joedunn025@gmail.com>
2026-02-22 05:23:53 -0600

    Update safety page

--- app/syllabus/page.tsx
+++ app/syllabus/page.tsx
@@ -1,6 +1,5 @@
 import SiteHeader from "../components/SiteHeader";
 import PageWrapper from "../components/PageWrapper";
-import Syllabus from "./parser";
 
 export const metadata = {
     title: "Syllabus",
@@ -11,9 +10,7 @@ export default function Page() {
         <PageWrapper>
             <SiteHeader />
             <main className="mt-6">
-                <div className="p-6 rounded-lg bg-white/60 dark:bg-zinc-900/60 border border-zinc-200 dark:border-zinc-800">
-                    <Syllabus />
-                </div>
+
             </main>
         </PageWrapper>
     );
