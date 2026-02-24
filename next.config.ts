36f371693fd1031e459d4a71810efa15782a98b1
Capttech <joedunn025@gmail.com>
2026-02-24 10:12:09 -0600

    save

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["localhost", "127.0.0.1", "172.20.137.28"]
 };
 
 export default nextConfig;
